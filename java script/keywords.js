a28c5fdb5aea62d9f9af02c9d6c396155ef3e984
Ramavathmadhunaik <130757454+Ramavathmadhunaik@users.noreply.github.com>
2023-04-14 17:47:02 +0530

    Delete java script directory

--- java script/keywords.js
+++
@@ -1,9 +0,0 @@
-document.write("<h2>KEY WORDS</h2>")
-let x;//decleration
-x=100;//instalisation
-document.write("x="+x+"<br>")//100
-x=200;//instalisation
-document.write("x="+x+"<br>")//200
-x=laptop;//re-linstalisation
-document.write("x="+x+"<br>")//laptop
-
